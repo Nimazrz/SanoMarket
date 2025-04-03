52ecbcfa965d09d43c8fd16b96205a22f2f14daa
Nimazrz <Nimaaa8413@gmail.com>
2025-04-03 15:17:48 +0330

    filtering,  ordering and search added

--- market/views.py
+++ market/views.py
@@ -4,14 +4,16 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import ProductSerializer, CommentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework import filters
 
 class ProductsViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ['name', 'description']
+    ordering_fields = '__all__'
 
 
 class CommentViewSet(viewsets.ModelViewSet):
