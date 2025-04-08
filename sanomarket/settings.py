a21526934f304c8f72b01f5003a2ffb0873d5c25
Nimazrz <Nimaaa8413@gmail.com>
2025-04-08 13:03:45 +0330

    added .env file

--- sanomarket/settings.py
+++ sanomarket/settings.py
@@ -96,11 +96,11 @@ WSGI_APPLICATION = 'sanomarket.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sanomarket',
-        'USER': 'sanomarket_admin',
-        'PASSWORD': 'adminformarket',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
